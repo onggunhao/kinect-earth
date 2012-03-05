6a5d89a5efa3b597e91249905bcee4180a3a4ab5
Huyen Tran <hey.huyen@gmail.com>
2012-03-04 19:07:31 -0800

    added super controller

---
+++ merged_stuff/SuperController.cs
@@ -0,0 +1,177 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using System.Windows.Controls;
+using Microsoft.Research.Kinect.Nui;
+using Coding4Fun.Kinect.Wpf;
+using WindowsInput;
+
+namespace SkeletalTracking
+{
+
+    class SuperController : SkeletonController
+    {
+        private MainWindow window;
+
+        // Variables to keep "time"
+        public int selectCount = 0;
+        int forwardCount = 0;
+
+        public SuperController(MainWindow win)
+            : base(win)
+        {
+            window = win;
+        }
+
+        public override void processSkeletonFrame(SkeletonData skeleton, Dictionary<int, Target> targets)
+        {
+
+            // Birdwatcher Menu is NOT up. Detect Navigation.
+            // Get right foot position
+            Point rightFootPosition;
+            Joint rightFoot = skeleton.Joints[JointID.FootRight];
+            rightFootPosition = new Point(rightFoot.Position.X, rightFoot.Position.Z);
+
+            // Get left foot position
+            Point leftFootPosition;
+            Joint leftFoot = skeleton.Joints[JointID.FootLeft];
+            leftFootPosition = new Point(leftFoot.Position.X, leftFoot.Position.Z);
+
+            double feetDifferential = leftFootPosition.Y - rightFootPosition.Y;
+
+            if (feetDifferential > 0.1)
+            {
+                // move forward slow (highlight 1 = middle)
+                targets[2].setTargetUnselected();
+                targets[3].setTargetUnselected();
+                targets[1].setTargetHighlighted();
+
+                if (feetDifferential > 0.35)
+                {
+                    // move forward fast (select 1)
+                    targets[1].setTargetSelected();
+                    InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_2); // faster
+                }
+                else
+                {
+                    InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_W); // regular
+                }
+
+            }
+            else if (feetDifferential < -0.1)
+            {
+                // move backward (select 3)
+                targets[1].setTargetUnselected();
+                targets[2].setTargetUnselected();
+                targets[3].setTargetSelected();
+                InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_S);
+            }
+            else
+            {
+                // stay put (select 2)
+                targets[1].setTargetUnselected();
+                targets[3].setTargetUnselected();
+                targets[2].setTargetSelected();
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_W);
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_S);
+            }
+
+            // Detect left-right shoulder navigation
+            // Gets right shoulder position
+            Joint rightShoulder = skeleton.Joints[JointID.ShoulderRight];
+            Point rightNav = new Point(rightShoulder.Position.X, rightShoulder.Position.Z);
+
+            Joint leftShoulder = skeleton.Joints[JointID.ShoulderLeft];
+            Point leftNav = new Point(leftShoulder.Position.X, leftShoulder.Position.Z);
+
+            double shoulderDifferential = leftNav.Y - rightNav.Y;
+
+            if (shoulderDifferential > 0.05)       // Right
+            {
+                targets[6].setTargetSelected();
+                targets[7].setTargetUnselected();
+                InputSimulator.SimulateKeyDown(VirtualKeyCode.LEFT);
+            }
+            else if (shoulderDifferential < -0.05)       // Left
+            {
+                targets[6].setTargetUnselected();
+                targets[7].setTargetSelected();
+                InputSimulator.SimulateKeyDown(VirtualKeyCode.RIGHT);
+            }
+            else
+            {
+                targets[6].setTargetUnselected();
+                targets[7].setTargetUnselected();
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.RIGHT);
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.LEFT);
+            }
+
+            // Detect Birdwatcher Gesture
+            // Get head position
+            Point headPosition;
+            Joint head = skeleton.Joints[JointID.Head];
+            headPosition = new Point(head.Position.X, head.Position.Y);
+
+            // Get right hand position
+            Point rightHandPosition;
+            Joint rightHand = skeleton.Joints[JointID.HandRight];
+            rightHandPosition = new Point(rightHand.Position.X, rightHand.Position.Y);
+
+            Point rightElbowPosition;
+            Joint rightElbow = skeleton.Joints[JointID.ElbowRight];
+            rightElbowPosition = new Point(rightElbow.Position.X, rightElbow.Position.Y);
+
+            // Get right shoulder position
+            Point rightShoulderPosition;
+            //Joint rightShoulder = skeleton.Joints[JointID.ShoulderRight];
+            rightShoulderPosition = new Point(rightShoulder.Position.X, rightShoulder.Position.Y);
+
+            //Calculate how far our right hand is from target 5 in both x and y directions
+            double deltaX = Math.Abs(rightHandPosition.X - headPosition.X);
+            double deltaY = Math.Abs(rightHandPosition.Y - headPosition.Y);
+
+            double headelbowXDifferential = rightElbowPosition.X - headPosition.X;
+
+            // y increases towards top
+            // hand > elbow > shoulder
+
+            if (deltaY < 0.03
+                    && rightHandPosition.Y > rightElbowPosition.Y
+                    && rightElbowPosition.Y > rightShoulderPosition.Y
+                    && deltaX < 0.3
+                    && headelbowXDifferential > 0.2)
+            {
+                // Birdwatcher! (highlight 4 and show 5)
+                InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_Y); // show augmented reality
+                //targets[4].showTarget();
+                targets[4].setTargetSelected();
+                //targets[5].showTarget();
+
+                // hide navigation targets 1,2,3
+                targets[1].setTargetUnselected();
+                targets[2].setTargetUnselected();
+                targets[3].setTargetUnselected();
+                //targets[1].hideTarget();
+                //targets[2].hideTarget();
+                //targets[3].hideTarget();
+                targets[6].setTargetUnselected();
+                targets[7].setTargetUnselected();
+                //targets[6].hideTarget();
+                //targets[7].hideTarget();
+            }
+            else
+            {
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_Y);
+            }
+        }
+
+        public override void controllerActivated(Dictionary<int, Target> targets)
+        {
+
+        }
+    }
+}
