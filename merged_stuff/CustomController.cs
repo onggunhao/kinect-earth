c979ac8171e49c9f6c4de9ab055038944632f267
Huyen Tran <hey.huyen@gmail.com>
2012-03-06 22:16:58 -0800

    huyen testing

--- merged_stuff/CustomController.cs
+++ merged_stuff/CustomController.cs
@@ -29,18 +29,18 @@ namespace SkeletalTracking
 
         public override void processSkeletonFrame(SkeletonData skeleton, Dictionary<int, Target> targets)
         {
-            
+
             if (targets[4].isSelected())
             {
                 // Birdwatcher Menu (targets 4, 5) is displayed
-                Joint rightHand = skeleton.Joints[JointID.HandRight].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
+                Joint leftHand = skeleton.Joints[JointID.HandLeft].ScaleTo(640, 480, window.k_xMaxJointScale, window.k_yMaxJointScale);
 
                 //Calculate how far our right hand is from target 5 in both x and y directions
-                double deltaX_right = Math.Abs(rightHand.Position.X - targets[5].getXPosition());
-                double deltaY_right = Math.Abs(rightHand.Position.Y - targets[5].getYPosition());
+                double deltaX_left = Math.Abs(leftHand.Position.X - targets[5].getXPosition());
+                double deltaY_left = Math.Abs(leftHand.Position.Y - targets[5].getYPosition());
 
                 //If we have a hit in a reasonable range, highlight the target5
-                if (deltaX_right < 20 && deltaY_right < 20)
+                if (deltaX_left < 20 && deltaY_left < 20)
                 {
                     targets[5].setTargetSelected();
                     selectCount++;
@@ -49,19 +49,9 @@ namespace SkeletalTracking
                         // target 5 selected, clear menu and return to Navigation (Targets 1,2,3)
                         // hide Targets 4,5
                         targets[4].setTargetUnselected();
-                        targets[4].hideTarget();
+                        //targets[4].hideTarget();
                         targets[5].setTargetUnselected();
                         targets[5].hideTarget();
-
-                        // show Targets 1,2,3
-                        //targets[1].setTargetUnselected();
-                        //targets[2].setTargetUnselected();
-                        //targets[3].setTargetUnselected();
-                        targets[1].showTarget();
-                        targets[2].showTarget();
-                        targets[3].showTarget();
-                        targets[6].showTarget();
-                        targets[7].showTarget();
 
                         // reset the count
                         selectCount = 0;
@@ -93,18 +83,19 @@ namespace SkeletalTracking
                     // move forward (highlight 1)
                     targets[2].setTargetUnselected();
                     targets[3].setTargetUnselected();
-                    targets[1].setTargetSelected();
+                    targets[1].setTargetHighlighted();
                     //InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_W); // regular
-                    
-                    if (feetDifferential > 0.8)
+
+                    if (feetDifferential > 0.35)
                     {
+                        targets[1].setTargetSelected();
                         InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_2); // faster
                     }
                     else
                     {
                         InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_W); // regular
                     }
-               
+
                 }
                 else if (feetDifferential < -0.1)
                 {
@@ -165,7 +156,7 @@ namespace SkeletalTracking
                 Joint rightHand = skeleton.Joints[JointID.HandRight];
                 rightHandPosition = new Point(rightHand.Position.X, rightHand.Position.Y);
 
-                 Point rightElbowPosition;
+                Point rightElbowPosition;
                 Joint rightElbow = skeleton.Joints[JointID.ElbowRight];
                 rightElbowPosition = new Point(rightElbow.Position.X, rightElbow.Position.Y);
 
@@ -173,7 +164,7 @@ namespace SkeletalTracking
                 Point rightShoulderPosition;
                 //Joint rightShoulder = skeleton.Joints[JointID.ShoulderRight];
                 rightShoulderPosition = new Point(rightShoulder.Position.X, rightShoulder.Position.Y);
-                
+
                 //Calculate how far our right hand is from target 5 in both x and y directions
                 double deltaX = Math.Abs(rightHandPosition.X - headPosition.X);
                 double deltaY = Math.Abs(rightHandPosition.Y - headPosition.Y);
@@ -199,40 +190,15 @@ namespace SkeletalTracking
                     targets[1].setTargetUnselected();
                     targets[2].setTargetUnselected();
                     targets[3].setTargetUnselected();
-                    targets[1].hideTarget();
-                    targets[2].hideTarget();
-                    targets[3].hideTarget();
                     targets[6].setTargetUnselected();
                     targets[7].setTargetUnselected();
-                    targets[6].hideTarget();
-                    targets[7].hideTarget();
                 }
             }
         }
 
         public override void controllerActivated(Dictionary<int, Target> targets)
         {
-            //targets[1].setTargetUnselected();
-            //targets[1].showTarget();
-            //targets[1].setTargetPosition(262, 30);
-            //targets[2].setTargetUnselected();
-            //targets[2].showTarget();
-            //targets[2].setTargetPosition(262, 150);
-            //targets[3].setTargetUnselected();
-            //targets[3].showTarget();
-            //targets[3].setTargetPosition(262, 270);
-            //targets[4].setTargetUnselected();
-            //targets[4].hideTarget();
-            //targets[4].setTargetPosition(400, 30);
-            //targets[5].setTargetUnselected();
-            //targets[5].hideTarget();
-            //targets[5].setTargetPosition(400, 150);
 
-            //// show left-right directional targets
-            //targets[6].setTargetUnselected();
-            //targets[6].showTarget();
-            //targets[7].setTargetUnselected();
-            //targets[7].showTarget();
         }
     }
 }
