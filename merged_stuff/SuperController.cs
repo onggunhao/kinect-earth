ee36d3d70900e1be644c698a2d7c61866d7bdd23
Huyen Tran <hey.huyen@gmail.com>
2012-03-07 19:23:59 -0800

    incorporated jetpack gesture

--- merged_stuff/SuperController.cs
+++ merged_stuff/SuperController.cs
@@ -23,6 +23,7 @@ namespace SkeletalTracking
 
         Boolean AugmentedRealityOn = false;
         double THRESH = 0.3;
+        double JetPackThresh = 0.05;
 
         public SuperController(MainWindow win)
             : base(win)
@@ -40,12 +41,15 @@ namespace SkeletalTracking
             if (!detectArmTurning(skeleton, targets))
             {
                 // Detect left-right shoulder turning navigation
-                detectShoulderTurning(skeleton, targets);
+                if (!detectShoulderTurning(skeleton, targets))
+                {
+                    // Detect JetPackUP
+                    detectJetPackUp(skeleton, targets);
+                }
             }
 
             // Detect Birdwatcher Gesture
             detectBirdwatcher(skeleton, targets);
-            
         }
 
         public override void controllerActivated(Dictionary<int, Target> targets)
@@ -249,6 +253,63 @@ namespace SkeletalTracking
                     AugmentedRealityOn = false;
                 }
             }
+        }
+
+        private void detectJetPackUp(SkeletonData skeleton, Dictionary<int, Target> targets)
+        {
+            targets[4].setTargetSelected();
+
+            // right side
+            Point rightHandPosition, rightElbowPosition, rightShoulderPosition;
+            Joint rightHand = skeleton.Joints[JointID.HandRight];
+            rightHandPosition = new Point(rightHand.Position.X, rightHand.Position.Y);
+            Joint rightElbow = skeleton.Joints[JointID.ElbowRight];
+            rightElbowPosition = new Point(rightElbow.Position.X, rightElbow.Position.Y);
+            Joint rightShoulder = skeleton.Joints[JointID.ShoulderRight];
+            rightShoulderPosition = new Point(rightShoulder.Position.X, rightShoulder.Position.Y);
+
+            // left side
+            Point leftHandPosition, leftElbowPosition, leftShoulderPosition;
+            Joint leftHand = skeleton.Joints[JointID.HandLeft];
+            leftHandPosition = new Point(leftHand.Position.X, leftHand.Position.Y);
+            Joint leftElbow = skeleton.Joints[JointID.ElbowLeft];
+            leftElbowPosition = new Point(leftElbow.Position.X, leftElbow.Position.Y);
+            Joint leftShoulder = skeleton.Joints[JointID.ShoulderLeft];
+            leftShoulderPosition = new Point(leftShoulder.Position.X, leftShoulder.Position.Y);
+
+            // shoulder elbow x difference
+            // shoulder hand x difference
+            double rightShoulderElbowDiffX = Math.Abs(rightShoulderPosition.X - rightElbowPosition.X);
+            double rightShoulderHandDiffX = Math.Abs(rightShoulderPosition.X - rightHandPosition.X);
+
+            // elbow hand y difference
+            double rightElbowHandDiffY = Math.Abs(rightElbowPosition.Y - rightHandPosition.Y);
+
+            // left side differences
+            double leftShoulderElbowDiffX = Math.Abs(leftShoulderPosition.X - leftElbowPosition.X);
+            double leftShoulderHandDiffX = Math.Abs(leftShoulderPosition.X - leftHandPosition.X);
+
+            // elbow hand y difference
+            double leftElbowHandDiffY = Math.Abs(leftElbowPosition.Y - leftHandPosition.Y);
+
+            if (rightShoulderElbowDiffX < JetPackThresh
+                && rightShoulderHandDiffX < JetPackThresh
+                && rightElbowHandDiffY < JetPackThresh
+                && leftShoulderElbowDiffX < JetPackThresh
+                && leftShoulderHandDiffX < JetPackThresh
+                && leftElbowHandDiffY < JetPackThresh)
+            {
+                targets[5].setTargetSelected();
+                InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_U);
+            }
+            else
+            {
+                targets[5].setTargetUnselected();
+                InputSimulator.SimulateKeyUp(VirtualKeyCode.VK_U);
+                InputSimulator.SimulateKeyDown(VirtualKeyCode.VK_J);
+            }
+
+
         }
     }
 }
