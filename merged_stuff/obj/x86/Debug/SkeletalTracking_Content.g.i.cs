a5f64a365f428f325e8b2d57f4e860397759cd05
strazz <strazzulla@gmail.com>
2012-03-11 15:46:21 -0700

    more resolution changes to gui

--- merged_stuff/obj/x86/Debug/SkeletalTracking_Content.g.i.cs
+++ merged_stuff/obj/x86/Debug/SkeletalTracking_Content.g.i.cs
@@ -8,6 +8,6 @@
 // </auto-generated>
 //------------------------------------------------------------------------------
 
-[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/penguins.jpg")]
+[assembly: System.Windows.Resources.AssemblyAssociatedContentFileAttribute("images/golden_gate.jpg")]
 
 
